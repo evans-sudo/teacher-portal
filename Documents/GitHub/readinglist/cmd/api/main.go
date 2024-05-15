827ad9f0434332779a84c5de86ec2fe28f0eca2d
evans-sudo <chiamaevans10@gmail.com>
2024-05-15 23:09:24 +0100

    readinglist

---
+++ Documents/GitHub/readinglist/cmd/api/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env string
+	dsn string
+}
+
+type application struct {
+	config config
+	logger *log.Logger
+}
+
+func main() {
+   var cfg config
+
+   flag.IntVar(&cfg.port, "port", 4000, "API server port")
+   flag.StringVar(&cfg.env, "env", "dev", "Environment (dev|stage|prod)")
+   flag.StringVar(&cfg.dsn, "db-dsn", os.Getenv("READINGLIST_DB_DSN"), "PostgreSQL DSN")
+   flag.Parse()
+
+
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	db, err := sql.Open("postgres", cfg.dsn)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	logger.Printf("database connection pool established")
+
+
+	
+	addr :=fmt.Sprintf(":%d", cfg.port)
+
+	srv := &http.Server{
+		Addr: addr,
+		Handler: app.route(),
+		IdleTimeout: time.Minute,
+		ReadTimeout: 10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+
+
+	}
+
+
+	logger.Printf("starting %s server on %s", cfg.env, addr)
+	err = srv.ListenAndServe()
+	logger.Fatal(err)
+}
